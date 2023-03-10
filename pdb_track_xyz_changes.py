6b5d9cb0a75031a25ea4616b49bf46e050caa3c4
adriangzlz97 <adrian.gonzalez.lopez@icm.uu.se>
2023-03-10 11:13:58 +0100

    Added descriptions to functions and classes

--- pdb_track_xyz_changes.py
+++ pdb_track_xyz_changes.py
@@ -32,9 +32,25 @@ class Atom:
         self.biso = biso
         self.xyz_change = xyz_change
     def print_info(self):
+        """
+        Prints the attributes of each atom tab separated. For testing purposes.
+        
+        """
         print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (self.atomid, self.element, self.altid, self.restyp, self.chainid, self.seqid, self.x, self.y, self.z, self.occ, self.biso, self.xyz_change))
 #Function to parse through the pdb file and obtain a list of atoms
 def get_atoms_from_pdb(file):
+    """
+    Parses through a pdb file and generates a list of atoms.
+
+    Inputs
+    ------
+    String indicating the pdb file to parse.
+
+    Returns
+    -------
+    List of atoms as Atom class.
+    """
+    file = open(file, 'r')
     lines = file.readlines()
     pdb = []
     count = 0
@@ -46,12 +62,33 @@ def get_atoms_from_pdb(file):
     return(pdb)
 import random
 def displace_xyz(pdb):
+    """
+    Displaces x, y and z by a random number between 1 and 10. For testing purposes.
+
+    Input
+    -----
+    Parsed pdb file : list of Atoms (class)
+    
+    """
     for atom in pdb:
-        atom.x += random.randint(1,5)
-        atom.y += random.randint(1,5)
-        atom.z += random.randint(1,5)
+        atom.x += random.randint(1,10)
+        atom.y += random.randint(1,10)
+        atom.z += random.randint(1,10)
 #Define function to compare atom distances and write into the atom attribute
 def compare_pdb_xyz(pdb1, pdb2):
+    """
+    Compares two lists of Atoms (class)
+
+    Inputs
+    ------
+    pdb1, pdb2 : List of Atoms (class)
+
+    Returns
+    -------
+    Modifies self.xyz_change from pdb1 atoms (class) based on the
+    x, y, z change between pdb1 and pdb2.
+
+    """
     for atom1 in pdb1:
         for atom2 in pdb2:
             #Make sure it is the same atom being compared
@@ -66,6 +103,18 @@ def compare_pdb_xyz(pdb1, pdb2):
                 atom1.xyz_change = xyz
 
 def find_max_res(pdb):
+    """
+    Finds the atom with most xyz_change and returns a list of these atoms sorted by the xyz_change.
+
+    Inputs
+    ------
+    pdb : List of Atoms (class)
+
+    Returns
+    -------
+    resi_list_max : list of Atoms from pdb with the largest xyz_change per residue.
+
+    """
     atom_p = Atom(0,0,0,0,0,0,0,0,0,0,0,0)
     resi = []
     resi_list_atom = []
@@ -91,7 +140,7 @@ def find_max_res(pdb):
     
 
 import copy
-file1 = open("test.pdb", 'r')
+file1 = "test.pdb"
 pdb1 = get_atoms_from_pdb(file1)
 pdb2 = copy.deepcopy(pdb1)
 displace_xyz(pdb2)
