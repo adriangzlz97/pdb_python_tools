c102b62ff6a1457b8c37f0bdcab61007296f60f1
adriangzlz97 <adrian.gonzalez.lopez@icm.uu.se>
2023-03-16 11:42:48 +0100

    Improved documentation

--- find_contacts.py
+++ find_contacts.py
@@ -8,18 +8,30 @@ from pdb_python_tools import find_contacts
 
 hetatm = 0
 hydrogens = 0
+
+# Check for flags
 for i in sys.argv:
     if i == "-HETATM":
         hetatm = i
     if i == "-ignore-hydrogens-false":
         hydrogens = i
+
+# Gather chainid
 chain = sys.argv[3]
+
+# Check format and parse with appropriate function
 if ".pdb" in sys.argv[1]:
     pdb = get_atoms_from_pdb(sys.argv[1], hetatm, hydrogens)
 elif ".cif" in sys.argv[1]:
     pdb = get_atoms_from_cif(sys.argv[1], hetatm, hydrogens)
+
+# Gather max distance to find
 distance = float(sys.argv[2])
+
+# Find the contacts within that distance
 atom_pairs = find_contacts(pdb, distance, chain)
+
+# Print table
 print("Chain1\tResidue1\tResidue1 number\tAtom1\tChain2\tResidue2\tResidue2 number\tAtom2\tDistance")
 for i in atom_pairs:
         print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (i[0].chainid, i[0].restyp, i[0].seqid, i[0].altid, i[1].chainid, i[1].restyp, i[1].seqid, i[1].altid, i[2]))
