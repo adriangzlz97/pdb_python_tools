07fa309f78d3a5d041897ddb4e57149a0d8d36e9
adriangzlz97 <adrian.gonzalez.lopez@icm.uu.se>
2024-06-11 11:38:31 +0200

    Add summary option

--- find_contacts_mpi.py
+++ find_contacts_mpi.py
@@ -15,6 +15,7 @@ size = comm.Get_size()
 hetatm = 0
 hydrogens = 0
 polar = False
+simple = False
 
 # Check for flags
 parser = argparse.ArgumentParser(
@@ -27,6 +28,7 @@ parser.add_argument('--distance', help='distance to check',type=float, required=
 parser.add_argument('-HET','--HETATM', action='store_true', dest='hetatm', help='include hetatms')
 parser.add_argument('-hy','--hydrogens', action='store_true', dest='hydrogens', help='include hydrogens')
 parser.add_argument('-p','--polar_only', action='store_true', dest='polar', help='check only polar')
+parser.add_argument('-s','--simple', action='store_true', dest='simple', help='simple output: just display the residues involved and closest distance')
 args = parser.parse_args()
 pdb = args.pdb
 chain = args.chain
@@ -34,6 +36,7 @@ distance = args.distance
 hetatm = args.hetatm
 hydrogens = args.hydrogens
 polar = args.polar
+simple = args.simple
 
 # Check format and parse with appropriate function
 if ".pdb" in pdb:
@@ -52,7 +55,30 @@ atom_pairs = find_contacts_resi_mpi(pdb, df_pdb, distance, chain, polar)
 
 # Print table on root
 if rank == 0:
-    print("Chain1\tResidue1\tResidue1 number\tAtom1\tChain2\tResidue2\tResidue2 number\tAtom2\tDistance")
-    for i in atom_pairs:
-        if len(i) == 3:
+    if simple:
+        n=0
+        for i in atom_pairs:
+            if n == 0:
+                atom_pairs_simple = [i]
+                n+=1
+            else:
+                count = -1
+                for j in atom_pairs_simple:
+                    check = False
+                    count += 1
+                    if i[0].seqid == j[0].seqid:
+                        if i[1].seqid == j[1].seqid:
+                            check = True
+                            if i[2] < j[2]:
+                                atom_pairs_simple[count][2] = i[2]
+                                break
+                if not check:
+                    atom_pairs_simple.append(i)
+        print("Residue1\tResidue1 number\tChain2\tResidue2\tResidue2 number\tDistance")
+        for i in atom_pairs_simple:
+            print("%s\t%s\t%s\t%s\t%s\t%s" % (i[0].restyp,i[0].seqid,i[1].chainid,i[1].restyp,i[1].seqid,i[2]))
+    else:
+        # Print table
+        print("Chain1\tResidue1\tResidue1 number\tAtom1\tChain2\tResidue2\tResidue2 number\tAtom2\tDistance")
+        for i in atom_pairs:
             print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (i[0].chainid, i[0].restyp, i[0].seqid, i[0].altid, i[1].chainid, i[1].restyp, i[1].seqid, i[1].altid, i[2]))
